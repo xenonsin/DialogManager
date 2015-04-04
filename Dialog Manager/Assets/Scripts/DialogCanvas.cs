5356227f70361e4f594713a9e455a0fa6874ec10
xenonsin <kenoiwasp@gmail.com>
2015-04-04 16:18:14 -0700

    Made Dialogue box and scroll

---
+++ Dialog Manager/Assets/Scripts/DialogCanvas.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogCanvas : MonoBehaviour
+{
+
+    public GameObject charPortrait;
+    public GameObject charName;
+    public GameObject charDialog;
+
+
+    private Image _portrait;
+    private Text _name;
+    private Text _dialog;
+	// Use this for initialization
+	void Start ()
+	{
+	    _portrait = charPortrait.GetComponent<Image>();
+	    _name = charName.GetComponent<Text>();
+	    _dialog = charDialog.GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BeginTalk(Sprite img, string name, string dialog)
+    {
+        SetPortrait(img);
+        SetName(name);
+        SetDialog(dialog);
+    }
+
+    public void SetPortrait(Sprite img)
+    {
+        _portrait.sprite = img;
+    }
+
+    public void SetName(string name)
+    {
+        _name.text = name;
+    }
+
+    public void SetDialog(string dialog)
+    {
+        _dialog.text = dialog;
+    }
+}
