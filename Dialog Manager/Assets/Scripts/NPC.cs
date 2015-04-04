9b5c12c94178a3ea97cdb08d5255879194d167ef
xenonsin <kenoiwasp@gmail.com>
2015-04-04 09:03:46 -0700

    Initial Commit

---
+++ Dialog Manager/Assets/Scripts/NPC.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+
+    public Sprite characterPortrait;
+    public string characterName;
+    public string charID;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
